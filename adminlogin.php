fc3ef64af36b608d5dea5ef7c205101923f112e3
rov15 <srvrov@yahoo.in>
2013-06-12 21:00:58 +0530

    Create adminlogin.php

---
+++ adminlogin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>profile</title>
+<link rel="icon" href="./favicon.ico">
+<link rel="stylesheet" href="stylesheets/style.css">
+<style>
+  	
+       body {background-image:url('./bg.jpg');}
+	
+</style>
+</head>
+<body>
+<center>
+<div id="monda_font">
+<br><br><br><br><br><br><br>
+<font color="#6699CC" size="15px">ADMIN LOGIN</font>
+<form action="./include/adminlog.php" name="form1" method="post">
+<table>
+<tr>
+<th>Login ID</th><td><input type="text" name="aid"></td>
+</tr>
+<tr>
+<th>Password</th><td><input type="password" name="apwd"></td>
+</tr>
+<tr><td></td></tr>
+<tr>
+<td colspan="2"><div align="center"><input name="alogin" type="submit" value="Enter to Admin Zone"></div></td>
+</tr>
+</table>
+</div>
+<a href="onfire.php"><div id="bckbtn" ><br><br><font color="#999999">Back</font></div></a>
+</center>
+</body>
+</html>
